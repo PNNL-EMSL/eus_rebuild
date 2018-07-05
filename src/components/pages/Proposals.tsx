cc406a72014f680ee72d8a4fe9f39c7c8214cf11
Pierce <kirsten.pierce@pnnl.gov>
2018-07-05 15:46:21 -0700

    Added Proposal Contents

--- src/components/pages/Proposals.tsx
+++ src/components/pages/Proposals.tsx
@@ -6,9 +6,12 @@ export default class Proposals extends Component {
             <div> 
                 <h1><strong>Get Started</strong></h1>
                 <p>Click the "Create New Proposal" button to the right to start filling out a new proposal.</p>
+
+                // Make as pop-up and double check that page is up-to-date
                 <p>Note: Submission of a proposal implies your agreement to the <a href="https://www.emsl.pnl.gov/using-emsl/terms.shtml">Terms and Conditions for Using EMSL</a></p>
             </div>
         );
     }
+    
 }
 
