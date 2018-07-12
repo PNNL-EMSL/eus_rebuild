af0fd4f7d556de12c33199191b245ac0bc3b1971
Pierce <kirsten.pierce@pnnl.gov>
2018-07-12 16:31:46 -0700

    Initial Messaging System setup

---
+++ src/components/pages/MessageSettings.tsx
@@ -0,0 +1,5 @@
+// import React, { Component} from 'react';
+// import { Carousel } from 'react-responsive-carousel';
+// import 'react-responsive-carousel/lib/styles/carousel.min.css';
+// import Marquee from 'react-smooth-marquee';
+
