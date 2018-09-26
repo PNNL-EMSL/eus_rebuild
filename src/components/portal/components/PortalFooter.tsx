a58194e650d67bf521dd53f6bdbb7a5d279ca84e
Kirsten Pierce <kirsten.pierce@pnnl.gov>
2018-09-26 08:18:12 -0700

    Footer text / icon placeholers

--- src/components/portal/components/PortalFooter.tsx
+++ src/components/portal/components/PortalFooter.tsx
@@ -4,7 +4,7 @@ import FooterBase from 'components/shared/components/FooterBase';
 
 const footer: string = css`
   padding: 5px 20px 5px 10px;
-  display: flex;
+  display: inline-block;
   flex: 0 0 auto;
   flex-direction: row;
   background-color: white;
@@ -13,10 +13,34 @@ const footer: string = css`
   width: 100%
 `;
 
+const footerRow: string=css`
+  display: inline-flex;
+`
+
 export default class PortalFooter extends FooterBase {
   renderContent() {
     return (
       <div className={footer}>
+        <div className={footerRow}>
+          <div>Contact Us</div>
+          <div>Terms & Conditions</div>
+          <div>Privacy</div>
+          <div>Frequently Asked Questions</div>
+          <div>emsl@pnnl.gov | 509.371.6003</div>
+        </div>
+        
+        <div className={footerRow}>
+          <div>EMSL Icon</div>
+          <div>PNNL Icon</div>
+          <div>DOE Icon</div>
+          <div>Connect with EMSL Icon</div>
+          <div>Facebook</div>
+          <div>Twitter</div>
+          <div>Instagram</div>
+          <div>LinkedIn</div>
+          <div>YouTube</div>
+          <div>OrcID</div>
+        </div>
         <p>
           Footer should be taken from the existing eusi.emsl.pnl.gov/Portal/ styles
         </p>
