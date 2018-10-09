8a71e0b16a8eb8dbdf2ceb006058d1c1cc8238c3
Degan, Michael G <michael.degan@pnnl.gov>
2018-10-09 09:17:23 -0700

    Removal of unneeded variable.

--- src/components/shared/components/wizard/ProgressHeader.tsx
+++ src/components/shared/components/wizard/ProgressHeader.tsx
@@ -6,10 +6,6 @@ export default class ProgressHeader extends Component<any, any> {
   }
 
   render() {
-    const content =
-      Object.keys(this.props.valids).forEach((index) => {
-      console.log('progress_step', index, this.props.valids[index]);
-    });
     return (
       <div>
         PROGRESS HEADER
