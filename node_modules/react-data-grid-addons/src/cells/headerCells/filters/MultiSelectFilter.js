990a14d726d15a5431b84c9fa3077bf634f30b20
Degan, Michael G <michael.degan@pnnl.gov>
2018-11-02 09:41:30 -0700

    Removal of node_moduels from git repository.

--- node_modules/react-data-grid-addons/src/cells/headerCells/filters/MultiSelectFilter.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import AutoCompleteFilter from './AutoCompleteFilter';
-
-export default (props) => {
-  return <AutoCompleteFilter {...props} multiSelection={true} />;
-};
