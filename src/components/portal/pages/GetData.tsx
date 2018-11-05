770a08fab429af7f4f2e63b7942ed4e7c8d5aab9
Davis, Prescott H <prescott.davis@pnnl.gov>
2018-11-05 09:41:57 -0800

    Added data tables and info button placeholder

--- src/components/portal/pages/GetData.tsx
+++ src/components/portal/pages/GetData.tsx
@@ -1,12 +1,113 @@
 import React from 'react';
+import {css} from 'emotion';
 import PortalPageBase from 'components/portal/pages/PortalPageBase';
-import {portalContentStyle} from 'styles/base';
+
+import info from 'images/info.png';
+import {portalContentStyle, colorWhite, colorDarkGreen, colorLightOrange, colorBurntOrange} from 'styles/base';
+
+const infoButton: string = css`
+  height: 17px;
+  margin-bottom: 3px;
+`;
+
+const dataTable: string = css`
+  width: 97%;
+  margin: 3px auto 7px auto;
+  thead>tr>td {
+    background-color: ${colorDarkGreen};
+    color: ${colorLightOrange};
+    font-size: 110%;
+    font-weight: bold;
+    padding: 4px;
+    border: 2px solid ${colorWhite};
+  }
+  tbody>tr>td {;
+    padding: 4px;
+    background-color: #ddd;
+    border: 2px solid ${colorWhite};
+    a, a:hover {
+      color: ${colorBurntOrange}
+    }
+  }
+`;
+
+const sampleData = [
+  {
+    id: '00001',
+    title: 'Sample Project 1',
+    data: [
+      {
+        instrument: 'Sample Instrument',
+        link: '#'
+      }
+    ],
+  },
+  {
+    id: '00002',
+    title: 'Sample Project 2',
+    data: [
+      {
+        instrument: 'Sample Instrument',
+        link: '#'
+      },
+      {
+        instrument: 'Sample Instrument',
+        link: '#'
+      }
+    ],
+  },
+  {
+    id: '00003',
+    title: 'Sample Project 3',
+    data: [
+      {
+        instrument: 'Sample Instrument',
+        link: '#'
+      }
+    ],
+  }
+];
 
 export default class GetData extends PortalPageBase {
+
+  renderData() {
+    const data = sampleData.map((project, index) =>
+      <div key={index}>
+        <strong>{project.id} {project.title}</strong>
+        <table className={dataTable}>
+          <thead>
+            <tr>
+              <td>Instrument</td>
+              <td>Data Link</td>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderInstrumentRows(project.data)}
+          </tbody>
+        </table>
+      </div>
+    );
+    return (data);
+  }
+
+  renderInstrumentRows(data) {
+    const rows = data.map((dataSet, index) =>
+      <tr key={index}>
+        <td>
+          {dataSet.instrument}
+        </td>
+        <td>
+          <a href={dataSet.link}>Get Data</a>
+        </td>
+      </tr>
+    );
+    return (rows);
+  }
+
   renderContent() {
     return (
       <div className={portalContentStyle}>
-        <h3>Get Data</h3>
+        <h3>Get Data <a><img className={infoButton} src={info} /></a></h3>
         <p>
           This page provides a list of your projects in numerical order and the associated
           instruments that have data available to download (i.e., it will not show all instruments
@@ -19,6 +120,7 @@ export default class GetData extends PortalPageBase {
         <p>
           To learn more about accessing these datasets, please refer to the Help document on this page.
         </p>
+        {this.renderData()}
       </div>
     );
   }
